7caf2ee43529713d1ad0af9447616f1cb476717e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-12-13 17:34:02 +0100

    feat: implemented card resendOTP method

--- index.ts
+++ index.ts
@@ -2,3 +2,11 @@ import ErcaspayClient from "./core";
 
 const client = new ErcaspayClient(process.env.SECRET_KEY as string);
 
+
+
+client.card.resendOTP({
+    transactionReference: "sdgsdkghlsfhsfh",
+    gatewayReference: "sgsdgsdlghsdghsdg",
+}).then((res) => {
+    console.log(res.responseBody); 
+});
