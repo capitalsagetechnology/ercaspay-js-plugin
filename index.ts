3abe2b129a957196e6b1bd1f42dbbd9102b16135
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-12-14 13:38:07 +0100

    feat: added getBankList method

--- index.ts
+++ index.ts
@@ -3,24 +3,6 @@ import ErcaspayClient from "./core";
 const client = new ErcaspayClient(process.env.SECRET_KEY as string);
 
 
-client.card.verifyTransaction("tREf").then((res) => {
-    console.log(res.responseBody) 
+client.ussd.getBankList().then((res) => {
+    console.log(res); 
 });
-
-client.checkout.verifyTransaction("tREf").then((res) => { 
-    console.log(res.responseBody);
-});
-
-
-// client.bankTransfer.initializeTransfer("tREf").then((res) => {
-//     console.log(res.responseBody)
-// })
-
-
-// client.ussd.initiateCode({
-//     transactionReference: "tREf",
-//     amount: 1000,
-//     bankName: "GTB"
-// }).then((res) => {
-//     console.log(res.responseBody.ussdCode)
-// })
