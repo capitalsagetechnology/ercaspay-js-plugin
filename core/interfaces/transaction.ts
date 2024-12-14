78446b8244b883f2174cd2e1bb4d347a52ad5cb7
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-12-14 15:41:53 +0100

    feat: added verify transaction method

--- core/interfaces/transaction.ts
+++ core/interfaces/transaction.ts
@@ -15,3 +15,4 @@ export interface IGetTransactionDetailsResponse {
   };
   paymentMethods: string[];
 }
+
--- core/transaction.ts
+++ core/transaction.ts
@@ -4,20 +4,30 @@ import type {
   IGetTransactionDetailsResponse,
 } from "./interfaces";
 
-
 export default class ErcaspayTransaction extends ErcaspayBase {
-    private readonly transactionBaseUrl = "/third-party/payment/details";
-    
+  private readonly transactionBaseUrl = "/third-party/payment";
 
-    public async getDetails(transactionReference: string) {
-        if (!transactionReference) {
-            throw new Error("Transaction reference is required");
-        }
-
-        const response = await this.Axios.get<
-          IBaseResponse<IGetTransactionDetailsResponse>
-        >(`${this.transactionBaseUrl}/${transactionReference}`);
-
-        return response.data;
+  public async getDetails(transactionReference: string) {
+    if (!transactionReference) {
+      throw new Error("Transaction reference is required");
     }
+
+    const response = await this.Axios.get<
+      IBaseResponse<IGetTransactionDetailsResponse>
+    >(`${this.transactionBaseUrl}/details/${transactionReference}`);
+
+    return response.data;
+  }
+
+  public async verify(transactionRef: string) {
+    if (!transactionRef) {
+      throw new Error("Transaction reference is required");
+    }
+
+    const response = await this.Axios.get<
+      IBaseResponse<IGetTransactionDetailsResponse>
+    >(`${this.transactionBaseUrl}/verify/${transactionRef}`);
+
+    return response.data;
+  }
 }
