dcd6c06fcc3db7674430df572161f9fbcd8ec21c
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-12-14 13:51:00 +0100

    feat: added get transaction details

--- index.ts
+++ index.ts
@@ -1,8 +1,3 @@
 import ErcaspayClient from "./core";
 
 const client = new ErcaspayClient(process.env.SECRET_KEY as string);
-
-
-client.ussd.getBankList().then((res) => {
-    console.log(res); 
-});
