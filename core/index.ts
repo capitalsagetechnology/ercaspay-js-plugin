77b301192f41cd3c9316419142a4a84f33d75821
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-12-14 15:21:06 +0100

    feat: added get transaction details method

--- core/index.ts
+++ core/index.ts
@@ -2,12 +2,15 @@ import ErcaspayCheckout from "./checkout";
 import ErcaspayCard from "./card";
 import ErcaspayBankTransfer from "./bank-transfer";
 import ErcaspayUSSD from "./ussd";
+import ErcaspayTransaction from "./transaction";
+
 
 export default class ErcaspayClient {
   public checkout: ErcaspayCheckout;
   public card: ErcaspayCard;
   public bankTransfer: ErcaspayBankTransfer;
   public ussd: ErcaspayUSSD;
+  public transaction: ErcaspayTransaction;
 
 
   constructor(secretKey: string, environment?: "production" | "development") {
@@ -15,5 +18,6 @@ export default class ErcaspayClient {
     this.card = new ErcaspayCard(secretKey, environment);
     this.bankTransfer = new ErcaspayBankTransfer(secretKey, environment);
     this.ussd = new ErcaspayUSSD(secretKey, environment);
+    this.transaction = new ErcaspayTransaction(secretKey, environment);
   }
 }
--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
 import ErcaspayClient from "./core";
 
 const client = new ErcaspayClient(process.env.SECRET_KEY as string);
+
+// client.transaction.getDetails("transactionReference").then((response) => {
+//     console.log(response.responseBody.)
+// });
