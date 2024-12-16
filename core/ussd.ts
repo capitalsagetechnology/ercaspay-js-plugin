804233d04c17a18de4440a0f94ad1546a5df809f
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-12-16 15:14:46 +0100

    docs: added Jsdocs to ussd class

--- core/ussd.ts
+++ core/ussd.ts
@@ -7,9 +7,37 @@ import type {
 } from "./interfaces";
 import { initiateCodeSchema } from "./../helpers/validations";
 
+/**
+ * ErcaspayUSSD handles operations related to initiating USSD codes, fetching supported banks,
+ * and canceling USSD transactions.
+ *
+ * @class ErcaspayUSSD
+ * @extends ErcaspayBase
+ */
 export default class ErcaspayUSSD extends ErcaspayBase {
+  /**
+   * Base URL for all USSD-related API endpoints.
+   * @private
+   * @readonly
+   */
   private readonly ussdBaseUrl = "/payment/ussd";
 
+  /**
+   * Initiates a USSD code for a specific transaction reference.
+   *
+   * @param {IInitiateCodeRequest} data - The details required to initiate the USSD code, including the transaction reference and bank name.
+   * @throws {Error} If validation of input data fails.
+   * @returns {Promise<IBaseResponse<IInitiateCodeResponse>>} A promise resolving to the USSD initiation response.
+   *
+   * @example
+   * const ussd = new ErcaspayUSSD();
+   * const data = {
+   *   transactionReference: "txn_063_imet_12345",
+   *   bankName: "GTBank",
+   * };
+   * const response = await ussd.initiateCode(data);
+   * console.log(response);
+   */
   public async initiateCode(data: IInitiateCodeRequest) {
     const values = await initiateCodeSchema.validateAsync(data);
 
@@ -34,6 +62,16 @@ export default class ErcaspayUSSD extends ErcaspayBase {
     return response.data;
   }
 
+  /**
+   * Fetches the list of supported banks for USSD transactions.
+   *
+   * @returns {Promise<IBaseResponse<string[]>>} A promise resolving to an array of supported bank names.
+   *
+   * @example
+   * const ussd = new ErcaspayUSSD();
+   * const bankList = await ussd.getBankList();
+   * console.log(bankList);
+   */
   public async getBankList() {
     const response = await this.Axios.get<IBaseResponse<string[]>>(
       `${this.ussdBaseUrl}/supported-banks`
