08db5bcfe7b8f40fbd793de89d9bbae9b6b2f4dd
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-12-16 08:49:52 +0100

    tests: finished cancel transaction test

--- tests/integration/transaction.test.ts
+++ tests/integration/transaction.test.ts
@@ -35,4 +35,34 @@ describe("Transaction Module Test 🧪", () => {
       expect(response.responseBody.transactionReference).toBeString();
     });
   });
+
+  describe("Cancel Transaction", async () => {
+    it("Should cancel a transaction and return a 200 status code, with appropriate typed response", async () => {
+      const initiateTransactionRequest = await client.transaction.initiate({
+        amount: 10000,
+        paymentReference: "R5md7gd9b4s3h2j5d67g",
+        paymentMethods: "card,bank-transfer,ussd,qrcode",
+        customerName: "Adedoyin Emmanuel",
+        customerEmail: "hi@adedoyinemmanuel.dev",
+        customerPhoneNumber: "09061626364",
+        redirectUrl: "https://github.com/adedoyin-emmanuel",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        currency: "NGN",
+        feeBearer: "customer",
+        metadata: {
+          firstname: "Temi",
+          lastname: "Girl",
+          email: "temigirl@mail.com",
+        },
+      });
+
+      const transactionRef =
+        initiateTransactionRequest.responseBody.transactionReference;
+
+      const response = await client.transaction.cancel(transactionRef);
+
+      expect(response.requestSuccessful).toBeTrue();
+      expect(response.responseBody).toBeObject();
+    });
+  });
 });
